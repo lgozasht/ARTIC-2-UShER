b8cd40e5476e97e2c8454592e29ab063acf53442
Landen Gozashti <40596750+lgozasht@users.noreply.github.com>
2021-03-08 13:20:44 -0800

    Add files via upload

--- Fasta2UShER.py
+++ Fasta2UShER.py
@@ -1,57 +1,103 @@
 import glob
-import os 
-import argparse 
+import os
+import argparse
 import re
-from sequenceAnalyzer import FastAreader
+from Bio import SeqIO
+import subprocess
 
+parser = argparse.ArgumentParser(
+    description='Generates merged VCF that ignores indels \
+                 and problematic sites and \
+                 recognizes missing data in addition to genotypes.')
 
-parser = argparse.ArgumentParser(description='Generates merged VCF that ignores indels and problematic sites and recognizes missing data in addition to genotypes.')
+print('\n\nFor more information on problematic sites see:\n\nNicola De Maio, '                                                                                                                                  
+      'Landen Gozashti, Yatish Turakhia, Conor Walker, Robert Lanfear, '
+      'Russell Corbett-Detig, and Nick Goldman, Issues with SARS-Cov-2 '
+      'sequencing data: Updated analysis with data from 12th June 2020, '
+      'Virological post 2020. '
+      'https://virological.org/t/issues-with-sars-cov-2-sequencing-data/473/12 '
+      '\n    and \nYatish Turakhia, Bryan Thornlow, Landen Gozashti, Angie '
+      'S. Hinrichs, Jason D. Fernandes, David Haussler, and Russell '
+      'Corbett-Detig, "Stability of SARS-CoV-2 Phylogenies", PLoS Genetics '
+      '2020.\n\n')
 
-print('\n\nFor more information on problematic sites see:\n\nNicola De Maio, Landen Gozashti, Yatish Turakhia, Conor Walker, Robert Lanfear, Russell Corbett-Detig, and Nick Goldman, Issues with SARS-Cov-2 sequencing data: Updated analysis with data from 12th June 2020, Virological post 2020. https://virological.org/t/issues-with-sars-cov-2-sequencing-data/473/12 \n    and \nYatish Turakhia, Bryan Thornlow, Landen Gozashti, Angie S. Hinrichs, Jason D. Fernandes, David Haussler, and Russell Corbett-Detig, "Stability of SARS-CoV-2 Phylogenies", bioRxiv pre-print 2020.\n\n')
-
-parser.add_argument('-inpath', nargs='?', required=True,
-                     help='Path to a directory containing all input multiple sequence alignments you wish to consider')
-parser.add_argument('-unaligned', nargs='?',const='T', default=None,
-                    help='Signifies that user provided fasta files have not been aligned to the reference. If specified, fasta2vcf uses mafft to perform multiple sequence alignments.')
-parser.add_argument('-auto_mask', nargs='?',const='T', default=None,
-                    help='Ignore problematic sites per our masking recomendations')
-parser.add_argument('-user_specified_mask', nargs='?', default=None,
-                    help='Path to VCF fle containing custom masking recomendations (please ensure VCF format is consistent with https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/master/problematic_sites_sarsCov2.vcf)')
-parser.add_argument('-outfile', nargs='?', required=True,
-                     help='Name of output file')
+# Add argparse arguments
+parser.add_argument(
+    '-inpath',
+    nargs='?',
+    required=True,
+    help='Path to a directory containing all input sequences you wish to consider')
+parser.add_argument(
+    '-unaligned',
+    nargs='?',
+    const='T',
+    default=None,
+    help='Signifies that user provided fasta files have not been \
+                    aligned to the reference. If specified, faToVcf \
+                    uses mafft to perform multiple sequence alignments.')
+parser.add_argument(
+    '-auto_mask',
+    nargs='?',
+    const='T',
+    default=None,
+    help='Ignore problematic sites per our masking recomendations')
+parser.add_argument(
+    '-user_specified_mask',
+    nargs='?',
+    default=None,
+    help='Path to VCF fle containing custom masking recomendations \
+                    (please ensure VCF format is consistent with \
+                    https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/master/problematic_sites_sarsCov2.vcf)')
+parser.add_argument('-reference', nargs='?', required=True,
+                    help='Path to reference fasta file. \
+                     The reference sequence header should be \
+                     identical to the reference sequence header \
+                     in the input msa file')
+parser.add_argument('-output', nargs='?', required=True,
+                    help='Name of output vcf file')
+parser.add_argument('-thread', nargs='?', required=False, default=1,
+                    help='Number of threads for msa')
 
 varDic = {}
 gDic = {}
 msaList = []
-
+headDic = {}
 args = vars(parser.parse_args())
-myReaderRef= FastAreader('MN908947.3.fa')
-for header, sequence in myReaderRef.readFasta():
-    reference = sequence.upper()
+filePath = '/'.join(os.path.realpath(__file__).split('/')[0:-1])
+# Check that faToVcf is in the same directory as Fasta2UShER
+if os.path.isfile('{0}/faToVcf'.format(filePath)) == False:
+    raise Exception("faToVcf must be in the same directory as Fasta2UShER")
 
+"""
 
-if args['unaligned'] != None:
-    
-    headCount = 0
-    for name in glob.glob('{0}/*'.format(args['inpath'])):
-        myReaderMSA= FastAreader(name)
-        for header, sequence in myReaderMSA.readFasta():
-            if header != 'MN908947.3':
-                headCount += 1
-                with open('{0}/temp.fa'.format(args['inpath']),'w') as f:
-                    f.write('>{0}\n{1}'.format(header,sequence))
-                os.system('cat MN908947.3.fa {0}/temp.fa > {0}/temp_premsa.fa'.format(args['inpath']))
-                os.system('mafft {0}/temp_premsa.fa  > {0}/{1}_{2}.fa'.format(args['inpath'],header.replace('/','_').replace(' ','_').replace('\t','_'),str(headCount)))
-                os.system('rm -r {0}/temp.fa'.format(args['inpath']))
-                os.system('rm -r {0}/temp_premsa.fa'.format(args['inpath']))
+If the user provided unaligned sequences, perform an MSA with mafft
+Otherwise, read the name of the provided msa file
 
-                msaList.append('{0}/{1}_{2}.fa'.format(args['inpath'],header.replace('/','_').replace(' ','_').replace('\t','_'),str(headCount)))
-    
+"""
+if args['unaligned'] is not None:
 
+    # create temporary file to harbor input sequences to MSA
+    allSeqs = {}
+    with open('temp.fa','w') as f:
+        for name in glob.glob('{0}/*'.format(args['inpath'])):
+            fasta_sequences = SeqIO.parse(open(name), 'fasta')
+            for fasta in fasta_sequences:
+                header, sequence = fasta.id, str(fasta.seq)
+                if header not in headDic:
+                    headDic[header] = ''
+                
+                    f.write('>{0}\n{1}\n'.format(header, sequence))
+    # Perform MSA
+    subprocess.call(
+        'mafft --thread {1} --auto --keeplength --addfragments temp.fa  {0} > {2}/inputMsa.fa'.format(
+            args['reference'],
+            args['thread'],
+            filePath),
+        shell=True)
+    msaName = '{0}/inputMsa.fa'.format(filePath)
+    os.system('rm -r temp.fa')
 else:
-    for name in glob.glob('{0}/*'.format(args['inpath'])):
-        msaList.append('{0}'.format(name))
-
+    msaName = ''.join(glob.glob('{0}/*'.format(args['inpath'])))
 seqDic = {}
 altCount = {}
 refCount = {}
@@ -59,103 +105,81 @@ refCount = {}
 
 """
 
-Retreave problematic sites
+Read in reference
 
 """
+with open(args['reference'], 'r') as f:
+    for line in f:
+        head = line.strip('>').strip()
+        break
+
+
+"""
+
+Run faToVcf
+
+"""
+
 probDic = {}
-if args['auto_mask'] != None:
 
-    os.system('rm -r problematic_sites_sarsCov2.vcf')
-    os.system('wget https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/master/problematic_sites_sarsCov2.vcf')
+# Retrieve recomended problematic sites if the user specied and run faToVcf
 
+if args['auto_mask'] is not None:
 
-
-    with open('problematic_sites_sarsCov2.vcf', 'r') as f:
-        for line in f:
-            if '#' in line:
-                pass
-            else:
-                cols = line.split('\t')
-
-                if cols[6].strip() == 'mask':
-                    probDic[str(cols[1].strip())] = ''
-
-elif args['user_specified_mask'] != None:
-    with open(args['user_specified_mask'], 'r') as f:
-        for line in f:
-            if '#' in line:
-                pass
-            else:
-                cols = line.split('\t')
-
-                if cols[6].strip() == 'mask':
-                    probDic[str(cols[1].strip())] = ''
-
-"""
-
-Identify variants and missing data in input alignments
-Ignore problematic sites
-
-"""
-
-for aln in msaList:
-    seqDic = {}
-    myReaderMSA= FastAreader(aln)
+    subprocess.call(
+        'wget https://raw.githubusercontent.com/W-L/ProblematicSites_SARS-CoV2/master/problematic_sites_sarsCov2.vcf',
+        shell=True)  # download masking recomendations
+    # if the user specified ".vcf" in the output file name, do not add ".vcf"
+    # extension
+    if '.vcf' in args['output']:
+        subprocess.call(
+            '{3}/faToVcf -maskSites={3}/problematic_sites_sarsCov2.vcf -ref=\"{0}\" {1} {2}'.format(
+                head, msaName, args['output'].strip()), shell=True)
+    # else add ".vcf" extension
+    else:  
+        subprocess.call(
+            '{3}/faToVcf -maskSites={3}/problematic_sites_sarsCov2.vcf -ref=\"{0}\" {1} {2}.vcf'.format(
+                head, msaName, args['output'].strip()), shell=True)
 
 
-    for header, sequence in myReaderMSA.readFasta():
-        
-        if header == 'MN908947.3' or sequence.replace('-','').upper() == reference:
-            refHead = header
-            refSeq = sequence.upper()
-        seqDic[header] = sequence.upper()
-        myTestSeq = sequence.upper()
+# Retrieve user provided problematic sites if the user specied and run
+# faToVcf
 
-    for i in range(len(refSeq)):
-        ref = refSeq[i]
-        if i not in refCount:
-            refCount[i] = 0
-        if i not in altCount:
-            altCount[i] = 0
-        if i not in gDic:
-            gDic[i] = {}
-        if i not in varDic:
-            varDic[i] = []
-     
-
-
-        for header in seqDic:
-            if seqDic[header][i] == ref or str(i+1) in probDic: #or ref == '-':
-                refCount[i] += 1
-                gDic[i][header] = 0
-            elif seqDic[header][i].upper() == 'N' or seqDic[header][i] == '-' or ref == '-':
-                #varDic[i][header] = '.'
-                gDic[i][header] = '.'
-               # print(i,header)
-            else:
-                if seqDic[header][i] not in varDic[i]:
-                    varDic[i].append(seqDic[header][i])
-                    
-                altCount[i] += 1
-
-                gDic[i][header] = varDic[i].index(seqDic[header][i])+1
-
-"""
-
-Write merged VCF
-
-"""
-
-with open('{0}'.format(args['outfile']),'w') as f:
-    f.write('##fileformat=VCFv4.2\n##source_20200913.1=vcf-merge(r953) {0}\n##sourceFiles_20200913.1={0}\n##nanopolish_window=MN908947.3:1-29902\n##INFO=<ID=TotalAltCalls,Number=1,Type=Integer,Description="The number of alternate allele calls for the variant">\n'.format(','.join(msaList)))
-    f.write('#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t{0}\n'.format('\t'.join(gDic[0].keys())))
-    for site in gDic:
-        if varDic[site]:
-            stringList = [str(x) for x in gDic[site].values()]
-            f.write('{0}\t{1}\t.\t{2}\t{3}\t.\t.\tAC={4}\tGT\t{5}\n'.format(refHead,str(site+1),refSeq[site].upper(),','.join(varDic[site]).upper(),altCount[site],'\t'.join(stringList)))
-    
-
-            
+elif args['user_specified_mask'] is not None:
+    # if the user specified ".vcf" in the output file name, do not add ".vcf"
+    # extension
+    if '.vcf' in args['output']:
+        subprocess.call(
+            '{3}/faToVcf -maskSites={3}/{0} -ref=\"{1}\" {2} {3}'.format(
+                args['user_specified_mask'],
+                head,
+                msaName,
+                args['output'],
+                filePath),
+            shell=True)
+    # else add ".vcf" extension
+    else:  
+        subprocess.call('{3}/faToVcf -maskSites={3}/{0} -ref=\"{1}\" {2} {3}.vcf'.format(
+            args['user_specified_mask'], head, msaName, args['output'], filePath), shell=True)
 
 
+# Else run faToVcf without masking
 
+else:
+
+    # if the user specified ".vcf" in the output file name, do not add ".vcf"
+    # extention
+    if '.vcf' in args['output']:
+        subprocess.call('{3}/faToVcf  -ref=\"{0}\" {1} {2}'.format(head,
+                                                                   msaName, args['output'], filePath), shell=True)
+    # else add ".vcf" extention
+    else:
+        subprocess.call('{3}/faToVcf  -ref=\"{0}\" {1} {2}.vcf'.format(head,
+                                                                       msaName, args['output'], filePath), shell=True)
+
+# remove masking recomendations file if it was downloaded
+if os.path.isfile('{0}/problematic_sites_sarsCov2.vcf'.format(filePath)):
+
+    subprocess.call(
+        'rm -r {0}/problematic_sites_sarsCov2.vcf'.format(filePath),
+        shell=True)
