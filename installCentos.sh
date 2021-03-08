2287076b4920328fd2f76430b8eb366c21010021
Landen Gozashti <40596750+lgozasht@users.noreply.github.com>
2021-03-08 13:13:36 -0800

    Create installCentos.sh

---
+++ installCentos.sh
@@ -0,0 +1,13 @@
+# install mafft
+wget https://mafft.cbrc.jp/alignment/software/mafft-7.471-gcc_fc6.x86_64.rpm
+sudo rpm -Uvh mafft-7.471-gcc_fc6.x86_64.rpm
+rm mafft-7.471-gcc_fc6.x86_64.rpm
+
+# install faToVcf
+rsync -aP rsync://hgdownload.soe.ucsc.edu/genome/admin/exe/linux.x86_64/faToVcf .
+chmod +x faToVcf
+mv faToVcf scripts/
+
+#install biopython
+pip3 install biopython
+
